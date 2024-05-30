0490246e2d0e1aa888dfd4d084a56bba41141bad
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-05-30 07:12:37 +0000

    new feature

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("How are you")
+function resolvesecond2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('success')
+            
+        }, 5000);
+        
+    })
+
+    
+}
+async function testing(params) {
+    console.log('name')
+    const result=await resolvesecond2()
+    console.log(result)
+    
+}
+testing()
